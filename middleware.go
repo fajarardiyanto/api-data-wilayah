35f7bfa47f8f3584edbc44cfd2be902f5b836eb9
fajar ardiyanto <fajarardiyanto6666@gmail.com>
2022-09-24 02:08:59 +0700

    middleware

--- middleware.go
+++ middleware.go
@@ -1,10 +1,47 @@
 package main
 
-import "net/http"
+import (
+	"github.com/fajarardiyanto/flt-go-router/interfaces"
+	"github.com/pkg/errors"
+	"net/http"
+)
 
-func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		next(w, r)
+func MiddlewareLogger() interfaces.MiddlewareFunc {
+	return func(next interfaces.Handler) interfaces.Handler {
+		return func(w http.ResponseWriter, r *http.Request) error {
+			return next(w, r)
+		}
+	}
+}
+
+func MiddlewareError() interfaces.MiddlewareFunc {
+	return func(next interfaces.Handler) interfaces.Handler {
+		return func(w http.ResponseWriter, r *http.Request) error {
+			if err := next(w, r); err != nil {
+				response := formatResponse(err)
+
+				if err = interfaces.JSON(w, response.Code, response); err != nil {
+					return err
+				}
+
+				GetLogger().Error(err).Quit()
+			}
+			return nil
+		}
+	}
+}
+
+// formatResponse returns response error format based on error type.
+func formatResponse(err error) *interfaces.APIResponseError {
+	switch et := errors.Cause(err).(type) {
+	case *interfaces.RequestError:
+		return interfaces.NewAPIResponseError(et.Error(), et.Status)
+	case interfaces.ValidationError:
+		return interfaces.NewAPIValidationError(et, http.StatusBadRequest)
+	default:
+		return interfaces.NewAPIResponseError(
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError,
+		)
 	}
 }
