7df3ca9cb387df4a1c30fd1988249b83633d983e
fajar ardiyanto <fajarardiyanto6666@gmail.com>
2022-09-24 02:07:57 +0700

    using module faltar router

--- main.go
+++ main.go
@@ -2,42 +2,18 @@ package main
 
 import (
 	"flag"
-	"github.com/gorilla/mux"
-	"net/http"
-	"runtime"
-	"time"
+	"github.com/fajarardiyanto/flt-go-router/lib"
 )
 
 var bindAddr string
 
 func main() {
-	go func() {
-		for {
-			var m runtime.MemStats
-			runtime.ReadMemStats(&m)
-
-			GetLogger().Info("Alloc = %v MiB (%v KiB)", bToMb(m.Alloc), bToKb(m.Alloc))
-			GetLogger().Info("TotalAlloc = %v MiB (%v KiB)", bToMb(m.TotalAlloc), bToKb(m.TotalAlloc))
-			GetLogger().Info("Sys = %v MiB (%v KiB)", bToMb(m.Sys), bToKb(m.Sys))
-			GetLogger().Info("NumGC = %v\n", m.NumGC)
-
-			time.Sleep(1 * time.Second)
-		}
-	}()
-
-	route := mux.NewRouter()
 	flag.StringVar(&bindAddr, "bind", ":8081", "bind addr")
 
-	route.HandleFunc("/regencies", SetMiddlewareJSON(HandlerRegencies))
+	router := lib.New("v1.0.0")
+	router.Use(MiddlewareLogger(), MiddlewareError())
 
-	GetLogger().Info("SERVER STARTING AT %s", bindAddr)
-	http.ListenAndServe(bindAddr, route)
-}
+	router.GET("/regencies", HandlerRegencies)
 
-func bToMb(b uint64) uint64 {
-	return b / 1024 / 1024
-}
-
-func bToKb(b uint64) uint64 {
-	return b / 1024
+	router.Run(bindAddr)
 }
